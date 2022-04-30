9fe793dc9b565d66f7bb61cbd548fcbb1c90b19d
yooki22 <yooki22@pos.idserve.net>
2022-04-30 18:42:41 +0900

    Create project

---
+++ NC1_happybank/NC1_happybank/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NC1_happybank
+//
+//  Created by 기윤수 on 2022/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NC1_happybank/NC1_happybank/NC1_happybankApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_happybankApp.swift
+//  NC1_happybank
+//
+//  Created by 기윤수 on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct NC1_happybankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
